72a7ef8672548144d60a69210a460314658458dc
nadiaputrirahmawati <nadiaputrirahman@gmail.com>
2025-09-07 21:43:39 +0700

    erorr build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import laravel from "laravel-vite-plugin";
 import Components from "unplugin-vue-components/vite";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
 
 export default defineConfig({
     plugins: [
@@ -18,9 +20,9 @@ export default defineConfig({
             },
         }),
         Components({
-            dts: true,
-            resolvers: [MotionResolver()],
-            dts: "resources/js/components.d.ts", // untuk VS Code auto-complete
+            dts: "resources/js/components.d.ts",
+            resolvers: [IconsResolver()],
         }),
+        Icons(),
     ],
 });
