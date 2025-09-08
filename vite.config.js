b7b5545632887bc031169a0ff03edb726eefdbc5
nadiaputrirahmawati <nadiaputrirahman@gmail.com>
2025-09-08 10:44:29 +0700

    perbaikan landing page responsive

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import laravel from "laravel-vite-plugin";
-import Components from "unplugin-vue-components/vite";
+// import Components from "unplugin-vue-components/vite";
+// import Icons from "unplugin-icons/vite";
+// import IconsResolver from "unplugin-icons/resolver";
 
 export default defineConfig({
     plugins: [
@@ -17,9 +19,10 @@ export default defineConfig({
                 },
             },
         }),
-        Components({
-            dts: "resources/js/components.d.ts",
-            resolvers: [IconsResolver()],
-        }),
+        // Components({
+        //     dts: "resources/js/components.d.ts",
+        //     resolvers: [IconsResolver()],
+        // }),
+        // Icons(),
     ],
 });
