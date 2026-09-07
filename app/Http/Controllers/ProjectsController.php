699699521b104589cc93f3f7c043f9d6461c7590
nadiaputrirahmawati <nadiaputrirahman@gmail.com>
2026-09-07 14:13:33 +0700

    update pagination project

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -16,7 +16,7 @@ class ProjectsController extends Controller
      */
     public function index()
     {
-        $projects = Project::orderBy('created_at', 'desc')->paginate(1);
+        $projects = Project::orderBy('created_at', 'desc')->paginate(5);
         return Inertia::render('Project/Index', [
             'projects' => $projects
         ]);
